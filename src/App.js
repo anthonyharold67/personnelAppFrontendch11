0577b6aa075ef5f7867ff522bfc4729b8e33c7f5
anthonyharold67 <93030628+anthonyharold67@users.noreply.github.com>
2023-05-13 00:24:54 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import PersonalContextProvider from "./contexts/PersonalContext";
 
 function App() {
   return (
-    <div style={{
-      backgroundImage: 'url(https://source.unsplash.com/random)',
-      backgroundRepeat: 'no-repeat',
-      backgroundColor: (t) =>
-        t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-      backgroundSize: 'cover',
-      backgroundPosition: "center",
-      // backgroundAttachment: "fixed",
-      height: "100vh",   
-      marginTop: 0
-    }}>
-    <AuthContextProvider>
-      {/* <PersonalContextProvider> */}
+    <div
+      style={{
+        backgroundImage: "url(https://source.unsplash.com/featured/)",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: (t) =>
+          t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900],
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        // backgroundAttachment: "fixed",
+        height: "100vh",
+        marginTop: 0,
+      }}>
+      <AuthContextProvider>
+        {/* <PersonalContextProvider> */}
         <AppRouter />
         <ToastContainer />
-      {/* </PersonalContextProvider> */}
-    </AuthContextProvider>
+        {/* </PersonalContextProvider> */}
+      </AuthContextProvider>
     </div>
   );
 }
